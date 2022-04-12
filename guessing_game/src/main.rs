b1ba7d7dd7386ac392d889dea8644e7101eeb5d1
kanemoto <kanemototomoki@gmail.com>
2022-04-12 20:44:46 +0900

    update: 数当てゲーム

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("数を当ててごらん");
@@ -7,11 +8,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("秘密の番号はこれ!: {}", secret_number);
+    println!("// 秘密の番号はこれ!: {}", secret_number);
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み込みに失敗しました");
-    println!("あなたの予想: {}", guess);
+
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+          .read_line(&mut guess)
+          .expect("行の読み込みに失敗しました");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}は小さすぎ！", guess),
+            Ordering::Greater => println!("{}は大きすぎ！", guess),
+            Ordering::Equal => {
+                println!("正解！");
+                break;
+            }
+        }
+        println!("予想を入力してね!");
+    }
 }
--- memo.md
+++ memo.md
@@ -5,3 +5,6 @@
 - `cargo run`でコンパイルから実行まで
 - `cargo check`バイナリは生成せずにビルドする watch的な
 - `cargo doc --open`でドキュメント生成&open
+
+- ライブラリクレートを追加するには`Cargo.toml`の`[dependencies]`セクションに追加する
+- `match`式は一般的なエラー処理で使う
