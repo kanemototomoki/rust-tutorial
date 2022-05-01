4b44356c1fecf40a45acc30baec304f5212b6c5d
kanemoto <kanemototomoki@gmail.com>
2022-05-01 22:01:12 +0900

    update: unwrap_or

--- memo/src/main.rs
+++ memo/src/main.rs
@@ -10,6 +10,11 @@ fn main() {
     //     Err(err) => println!("error: {}", err)
     // }
 
+    /**
+     * unwrap_or
+     * Ok()だった場合はそのまま展開
+     * Err()の場合は与えた引数を返す
+     */
     println!("result: {}", result.unwrap_or(-1));
     let result: Result<i32, String> = Err("fatal error".to_string());
     println!("result: {}", result.unwrap_or(-1));
